24dccdaf07a927113b23da10f1a4236ff2209d6f
Mano <manoaway@gmail.com>
2020-11-29 16:46:17 +0100

    add tableFill function

--- search.js
+++ search.js
@@ -21,43 +21,41 @@ fetch(endpoint)
     const table = document.getElementById('table');
 
 
+    const fillTable = () => {
+      data.results.forEach((x, i) => {
 
-    data.results.forEach((x, i) => {
-
-        //create first column and display names
-        const row1 = table.insertRow(i + 1);
-        const col1 = row1.insertCell(0);
-
-
-        col1.innerHTML = x.name
-
-        //create second column and display images
-
-        const col2 = row1.insertCell(1);
-
-        const oImg = document.createElement("img");
-        oImg.setAttribute('src', x.image);
-        oImg.setAttribute('alt', 'na');
-        oImg.setAttribute('height', '100px');
-        oImg.setAttribute('width', 'auto');
-        col2.appendChild(oImg)
-
-        //create third column and display gender
-        const col3 = row1.insertCell(2);
-        col3.innerHTML = x.gender
-
-        //create third column and display gender
-        const col4 = row1.insertCell(3);
-        col4.innerHTML = x.species
-
-        //create third column and display gender
-        const col5 = row1.insertCell(4);
-        col5.innerHTML = x.episode.map(episode => episode.substring(40))
-      }
-
-    )
+          //create first column and display names
+          const row1 = table.insertRow(i + 1);
+          const col1 = row1.insertCell(0);
 
 
+          col1.innerHTML = x.name
 
+          //create second column and display images
 
-  })
+          const col2 = row1.insertCell(1);
+
+          const oImg = document.createElement('img');
+          oImg.setAttribute('src', x.image);
+          oImg.setAttribute('alt', 'na');
+          oImg.setAttribute('height', '100px');
+          oImg.setAttribute('width', 'auto');
+          col2.appendChild(oImg);
+
+          //create third column and display gender
+          const col3 = row1.insertCell(2);
+          col3.innerHTML = x.gender;
+
+          //create third column and display gender
+          const col4 = row1.insertCell(3);
+          col4.innerHTML = x.species;
+
+          //create third column and display gender
+          const col5 = row1.insertCell(4);
+          col5.innerHTML = x.episode.map(episode => episode.substring(40));
+        }
+      );
+    };
+      fillTable()
+
+  });
